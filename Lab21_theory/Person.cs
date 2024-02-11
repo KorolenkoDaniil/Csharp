0f19dd1c5176fb11ead13e8502735f4e7821534e
korolenko_danila <korolenko.danila1@gmail.com>
2024-02-11 12:38:27 +0300

    Commit with theory of 21 lab

---
+++ Lab21_theory/Person.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Lab_21_theory
+{
+    class Person
+    {
+        public string Name { get; }
+        public int Age { get; set; }
+        public string Company { get; set; }
+        public Person(string name, int age, string company)
+        {
+            Name = name;
+            Age = age;
+            Company = company;
+        }
+    }
+}
