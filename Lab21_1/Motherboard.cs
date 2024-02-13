04625df5a2f7b714be194ea104ee2fabf4b8582b
korolenko_danila <korolenko.danila1@gmail.com>
2024-02-13 11:14:58 +0300

    deleted:    .vs/C#/FileContentIndex/08784bd5-cfed-4bdb-bdf6-9504d0bd7b3d.vsidx 	renamed:    .vs/C#/FileContentIndex/310c24f5-9fe6-471c-9312-12724ec2e8f1.vsidx -> .vs/C#/FileContentIndex/140e4232-046c-4c4c-90a9-aa03692f94d1.vsidx 	deleted:    .vs/C#/FileContentIndex/27a11389-8f17-45d7-a428-127af4886636.vsidx 	new file:   .vs/C#/FileContentIndex/64ee5de3-505f-4b98-8b6d-7b0999ed782d.vsidx 	new file:   .vs/C#/FileContentIndex/81ef0110-d886-47ea-954c-de6c4edd90df.vsidx 	new file:   .vs/C#/FileContentIndex/89f13fc2-113d-4580-8a95-0ec45bd50370.vsidx 	modified:   .vs/C#/v17/.suo 	modified:   C#.sln 	new file:   Lab21_1.rar 	new file:   Lab21_1/App.config 	new file:   Lab21_1/Lab21_1.csproj 	new file:   Lab21_1/Motherboard.cs 	new file:   Lab21_1/Program.cs 	new file:   Lab21_1/Properties/AssemblyInfo.cs 	new file:   Lab21_1/Validator.cs 	new file:   Lab21_1/bin/Debug/Lab21_1.exe 	new file:   Lab21_1/bin/Debug/Lab21_1.exe.config 	new file:   Lab21_1/bin/Debug/Lab21_1.pdb 	new file:   Lab21_1/bin/Debug/Motherboard.xml 	new file:   Lab21_1/bin/Debug/fff.xml 	new file:   Lab21_1/bin/Debug/id.txt 	new file:   Lab21_1/bin/Debug/w.xml 	new file:   Lab21_1/obj/Debug/.NETFramework,Version=v4.7.2.AssemblyAttributes.cs 	new file:   Lab21_1/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache 	new file:   Lab21_1/obj/Debug/Lab21_1.csproj.AssemblyReference.cache 	new file:   Lab21_1/obj/Debug/Lab21_1.csproj.CoreCompileInputs.cache 	new file:   Lab21_1/obj/Debug/Lab21_1.csproj.FileListAbsolute.txt 	new file:   Lab21_1/obj/Debug/Lab21_1.exe 	new file:   Lab21_1/obj/Debug/Lab21_1.pdb 	modified:   Lab21_theory/obj/Debug/DesignTimeResolveAssemblyReferencesInput.cache

---
+++ Lab21_1/Motherboard.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab21_1
+{
+    internal class Motherboard
+    {
+        public int id;
+        public string name;
+        public string mark;
+        public int amountOfRAM;
+        public decimal price;
+
+        public Motherboard() { }
+        public Motherboard(params string[] args) {
+            Name = args[0];
+            Mark = args[1];
+            AmountOfRAM = int.Parse(args[2]);
+            Price = decimal.Parse(args[3]);
+        }
+
+        public string Name
+        {
+            get => name;
+            set => name = Validator.ValidateVarchar(value, 40) ? 
+                value: throw new Exception("проверьте данные, которые вы ввели");
+        }
+
+        public string Mark
+        {
+            get => mark;
+            set => mark = Validator.ValidateVarchar(value, 40) ?
+                value : throw new Exception("проверьте данные, которые вы ввели");
+        }
+        public int AmountOfRAM
+        {
+            get => amountOfRAM;
+            set => amountOfRAM = Validator.ValidateNumber(value) ?
+                value : throw new Exception("проверьте данные, которые вы ввели");
+        }
+        public decimal Price
+        {
+            get => price;
+            set => price = Validator.ValidatePrice(value) ?
+                value : throw new Exception("проверьте данные, которые вы ввели");
+        }
+        public int ID
+        {
+            get => id;
+            set => id = Validator.ValidateNumber(value) ?
+                value : throw new Exception("проверьте данные, которые вы ввели");
+        }
+
+    }
+}
---
+++ Lab21_1/Program.cs
@@ -0,0 +1,268 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Lab21_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlDocument xdoc = new XmlDocument();
+            string filePath = "Motherboard.xml";
+
+            if (!File.Exists("id.txt"))
+            {
+                Create();
+            } 
+            string stringID = File.ReadAllText("id.txt").ToString();
+            int numberofID = string.IsNullOrEmpty(stringID) ? 1 : int.Parse(stringID);
+            
+            if (numberofID == 1)
+            {
+                File.WriteAllText("id.txt", $"{1}");
+            }
+            
+
+            while (true)
+            {
+                int n = 1;
+
+                do
+                {
+                    Console.WriteLine("1 - создание документа");
+                    Console.WriteLine("2 - выбор файла для работы");
+                    Console.WriteLine("3 - чтение объектов и добавление новых");
+                    Console.WriteLine("4 - удаление объекта");
+                    Console.WriteLine("5 - ");
+                    Console.WriteLine("0 - выход");
+
+                    Console.WriteLine("выберите пункт меню ");
+                    n = int.Parse(Console.ReadLine());
+                } while (n < 0 || n > 5);
+
+                if (n == 0)
+                {
+                    break; // Exit the loop when the user selects "0"
+                }
+
+                switch (n)
+                {
+                    case 1:
+                        {
+                            MakeNewFile(ref xdoc, ref filePath);
+                            break;
+                        }
+                    case 2:
+                        {
+                            ChoiceOfFile(ref xdoc, ref filePath);
+                            break;
+                        }
+                    case 3:
+                        {
+                            xdoc.Load(filePath);
+                            PrintNodes(ref xdoc);
+
+                            Console.ForegroundColor = ConsoleColor.Green;
+                            Console.WriteLine("1 - добавить элемент");
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine("2 - продолжить без добавления элемента");
+                            Console.ResetColor();
+                            int a = int.Parse(Console.ReadLine());
+                            if (a == 1) NewElement(ref xdoc, ref filePath, ref numberofID);
+                           
+                            break;
+                        }
+                    case 4:
+                        {
+                            DeleteFile(ref xdoc,ref filePath, ref numberofID);
+                            break;
+                        }
+                    case 5:
+                        {
+                            xdoc.Load(filePath);
+
+                            break;
+                        }
+                }
+            }
+        }
+
+        public static void PrintNodes(ref XmlDocument xdoc)
+        {
+            foreach (XmlNode personNode in xdoc.DocumentElement.ChildNodes)
+            {
+                if (personNode.Attributes.Count > 0)
+                {
+                    foreach (XmlAttribute xmlAttribute in personNode.Attributes)
+                    {
+                        Console.WriteLine($"{xmlAttribute.Name} {xmlAttribute.InnerText}");
+                    }
+                } 
+                 
+                if (personNode.HasChildNodes)
+                {
+                    foreach (XmlNode dataNode1 in personNode.ChildNodes)
+                    {
+                        Console.WriteLine($"{dataNode1.Name}: {dataNode1.InnerText}");
+                    }
+                    Console.WriteLine("--------------------------");
+                }
+            }
+        }
+
+
+
+
+        public static void Create()
+        {
+            File.Create("id.txt");
+        }
+
+
+        public static void NewElement(ref XmlDocument xdoc, ref string filePath, ref int numberofID)
+        {
+
+            xdoc.Load(filePath);
+            XmlElement xRoot = xdoc.DocumentElement; //взяли корневой
+
+            XmlElement child1 = xdoc.CreateElement("Motherboard"); //новый узел нового элемента
+            child1.SetAttribute("id", $"{numberofID}");
+            xRoot.AppendChild(child1);
+
+            Console.WriteLine("введите название материнской платы");
+            string name = Console.ReadLine();
+            XmlElement nameElem = xdoc.CreateElement("name");
+            nameElem.InnerText = name;
+            child1.AppendChild(nameElem);
+
+            Console.WriteLine("введите марку материнской платы");
+            string mark = Console.ReadLine();
+            XmlElement markElem = xdoc.CreateElement("mark");
+            markElem.InnerText = mark;
+            child1.AppendChild(markElem);
+
+            Console.WriteLine("введите количство памяти в материнской платы");
+            int amountOfRAM = int.Parse(Console.ReadLine());
+            XmlElement amountOfRAMElem = xdoc.CreateElement("amountOfRAM");
+            amountOfRAMElem.InnerText = amountOfRAM.ToString();
+            child1.AppendChild(amountOfRAMElem);
+
+            Console.WriteLine("введите цену материнской платы");
+            string price = Console.ReadLine();
+            XmlElement priceElem = xdoc.CreateElement("price");
+            priceElem.InnerText = price.ToString();
+            child1.AppendChild(priceElem);
+
+
+
+            xdoc.Save(filePath);
+
+            numberofID++;
+            File.WriteAllText("id.txt", $"{numberofID}");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("элемент сохранен");
+            Console.ResetColor();
+
+        }
+
+
+
+
+        public static void ChoiceOfFile(ref XmlDocument xdoc, ref string filePath)
+        {
+            string path = "D:\\C#\\Lab21_1\\bin\\Debug";  //исправь на полный путь папки Lab21_1\\bin\\Debug
+            DirectoryInfo thisDirectory = new DirectoryInfo(path);
+
+            FileInfo[] subDirectories = thisDirectory.GetFiles();
+            List<FileInfo> XMLsubDirectories = new List<FileInfo>();
+
+            Console.WriteLine(subDirectories.Length);
+            int k = 0;
+            foreach (FileInfo subDirectory in subDirectories)
+            {
+               
+                if (subDirectory.Extension.ToLower() == ".xml")
+                {
+                    k++;
+                    Console.WriteLine($"{k} - {subDirectory.Name}");
+                    XMLsubDirectories.Add(subDirectory);
+                }
+            }
+
+            if (k == 0)
+            {
+                Console.WriteLine("перед работой с файлом создайте его");
+                return;
+            }
+            int number;
+            try
+            {
+                Console.WriteLine("введите номер файла, который вы хотите открыть ");
+                number = int.Parse(Console.ReadLine());
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("что-то пошло не так, например такой файл не существует");
+                return;
+            }
+
+
+            filePath = XMLsubDirectories[number - 1].FullName;
+            Console.WriteLine("файл для работы выбран");
+        }
+
+
+        public static void MakeNewFile(ref XmlDocument xdoc, ref string filePath)
+        {
+            try
+            {
+                Console.WriteLine("введите название нового файла");
+                filePath = Console.ReadLine() + ".xml";
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("что-то пошло не так, например такой файл уже существует");
+                Console.ReadKey();
+            }
+
+            XmlElement root = xdoc.CreateElement("Motherboards");
+            xdoc.AppendChild(root);
+
+            xdoc.Save(filePath);
+
+            Console.WriteLine($"новый файл с названием {filePath} создан", ConsoleColor.Red);
+        }
+
+
+        public static void DeleteFile(ref XmlDocument xdoc, ref string filePath, ref int numberofID)
+        {
+            xdoc.Load(filePath);
+            XmlElement xRoot = xdoc.DocumentElement;
+
+            //xRoot.RemoveAll();     удаление всех узлов
+            //XmlNode f = xRoot.FirstChild;      удаление верхнего элемента  
+
+            PrintNodes(ref xdoc);
+
+            Console.WriteLine("выберите индекс элемента, который надо удалить");
+            int index = int.Parse(Console.ReadLine());
+                
+
+            XmlNode nodeToDelete = xRoot.SelectSingleNode($"//*[@id='{index}']");
+
+            // Удалить узел
+            if (nodeToDelete != null)
+            {
+                xRoot.RemoveChild(nodeToDelete);
+                numberofID--;
+            }
+           
+
+            xdoc.Save(filePath);
+        }
+
+    }
+}
